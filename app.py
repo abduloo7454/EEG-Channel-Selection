66c967559582a6936c61d4f0f073ee467d2dc3c3
Abdullah <55324025+abduloo7454@users.noreply.github.com>
2024-08-07 20:34:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-@st.cache
+
 st.set_page_config(page_title="EEG Channel Selection", layout="wide")
 
 BCI_Competition_IV_I_Calib_Subject-a = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-a.xlsx")
