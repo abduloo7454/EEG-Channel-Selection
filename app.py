3516e69d2b5f8d6ea1aab20268f7a55be41abd2b
Abdullah <55324025+abduloo7454@users.noreply.github.com>
2024-08-07 20:36:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import streamlit as st
 
 st.set_page_config(page_title="EEG Channel Selection", layout="wide")
 
-BCI_Competition_IV_I_Calib_Subject-a = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-a.xlsx")
-BCI_Competition_IV_I_Calib_Subject-b = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-b.xlsx")
-BCI_Competition_IV_I_Calib_Subject-f = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-f.xlsx")
+df1 = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-a.xlsx")
+df2 = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-b.xlsx")
+df3 = pd.read_excel("G-EEGCS-Streamlit/IV_I_Calib_Subject-f.xlsx")
 
-BCI_Competition_IV_2a_Subject-1 = pd.read_excel("G-EEGCS-Streamlit/IV_2a_Subject-1.xlsx")
-BCI_Competition_IV_2a_Subject-2 = pd.read_excel("G-EEGCS-Streamlit/IV_2a_Subject-2.xlsx")
-BCI_Competition_IV_2a_Subject-3 = pd.read_excel("G-EEGCS-Streamlit/IV_2a_Subject-3.xlsx")
+df4 = pd.read_excel("G-EEGCS-Streamlit/IV_2a_Subject-1.xlsx")
+df5 = pd.read_excel("G-EEGCS-Streamlit/IV_2a_Subject-2.xlsx")
+df6 = pd.read_excel("G-EEGCS-Streamlit/IV_2a_Subject-3.xlsx")
 
-BCI_Competition_III_V_Subject-1 = pd.read_excel("G-EEGCS-Streamlit/III_V_Subject-1.xlsx")
-BCI_Competition_III_V_Subject-2 = pd.read_excel("G-EEGCS-Streamlit/III_V_Subject-2.xlsx")
+df7 = pd.read_excel("G-EEGCS-Streamlit/III_V_Subject-1.xlsx")
+df8 = pd.read_excel("G-EEGCS-Streamlit/III_V_Subject-2.xlsx")
 
-BCI_Competition_II_Ia = pd.read_excel("G-EEGCS-Streamlit/II_Ia.xlsx")
-BCI_Competition_II_Ib = pd.read_excel("G-EEGCS-Streamlit/II_Ib.xlsx")
+df9 = pd.read_excel("G-EEGCS-Streamlit/II_Ia.xlsx")
+df10 = pd.read_excel("G-EEGCS-Streamlit/II_Ib.xlsx")
 
 # Display the filtered data
-st.dataframe(BCI_Competition_III_V_Subject-1)
+st.dataframe(df1)
